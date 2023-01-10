ebb805408d3ab9ee9e4386ff9fdba20941bce66a
Kene <kayyungx@gmail.com>
2023-01-10 15:49:16 +0100

    Add defualt parameter for multiplayer

--- Game_Author/game.rb
+++ Game_Author/game.rb
@@ -4,7 +4,7 @@ require_relative '/item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at, :publish_date
 
-  def initialize(multiplayer, last_played_at, publish_date, id , archived: false)
+  def initialize(multiplayer: true, last_played_at, publish_date, id , archived: false)
     super(publish_date, id, archived: archived)
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
