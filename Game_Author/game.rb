0555152f6070ec8afee32f475f94e08364b72215
Kene <kayyungx@gmail.com>
2023-01-10 16:28:51 +0100

    Change parameters position

--- Game_Author/game.rb
+++ Game_Author/game.rb
@@ -4,13 +4,13 @@ require_relative './item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at, :publish_date
 
-  def initialize(multiplayer: true, last_played_at, publish_date, id , archived: false)
-     super(publish_date, id, archived: archived)
-     @multiplayer = multiplayer
-     @last_played_at = Date.parse(last_played_at)
+  def initialize(last_played_at, publish_date, id, archived: false, multiplayer: true)
+    super(publish_date, id, archived: archived)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse(last_played_at)
   end
 
   def can_be_archived?
-     super && ((Date.today.year - @last_played_at.year) > 2)
+    super && ((Date.today.year - @last_played_at.year) > 2)
   end
 end
