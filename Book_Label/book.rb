ddd723a969de39bfe33b6e58753688b0159ba849
devMunyi <samunyi90@gmail.com>
2023-01-11 09:33:20 +0300

    Update Book class

--- Book_Label/book.rb
+++ Book_Label/book.rb
@@ -1,5 +1,10 @@
-class Book
+require_relative './item'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
   def initialize(publisher, cover_state)
+    super(date)
     @publisher = publisher
     @cover_state = cover_state
   end
