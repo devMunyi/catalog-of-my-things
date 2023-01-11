116a1f1064143807a16ae029e8a4619fcdcc6f51
Zuhaib Amjad <umar0423@gmail.com>
2023-01-11 15:46:37 +0500

    Add unit tests for MusicAlbum and Genre class

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,1 +1,17 @@
-puts 'To be implemented by Zuhaib'
+require_relative '../MusicAlbum_Genre/genre'
+require_relative '../item'
+
+describe Genre do
+  let(:genre) { Genre.new 'Hiphop' }
+  describe '#new' do
+    it 'should be an instance of Genre' do
+      expect(genre).to be_an_instance_of(Genre)
+    end
+
+    it 'should add an item' do
+      item = Item.new('22-12-2022')
+      genre.add_item(item)
+      expect(genre.items[0]).to be_an_instance_of(Item)
+    end
+  end
+end
