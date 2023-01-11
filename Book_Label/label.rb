6f44b834537968ddabe3876af3e759e99d382e28
devMunyi <samunyi90@gmail.com>
2023-01-11 09:08:23 +0300

    Add Label class properties

--- Book_Label/label.rb
+++ Book_Label/label.rb
@@ -1,3 +1,12 @@
-class Label
-  puts 'To be implemented in the next pull request'
+require_relative './item'
+
+class Label < Item
+  attr_accessor :title, color
+  attr_reader :id, items
+  def initialize(title, color)
+    @id = Random.rand(1..1000)
+    @title = title
+    @color = color
+    @items = []
+  end
 end
