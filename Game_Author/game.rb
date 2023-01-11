de9f78826ff87b68bd81e07b17e02e8125f19542
Kene <kayyungx@gmail.com>
2023-01-11 11:35:19 +0100

    Add games unit test

--- Game_Author/game.rb
+++ Game_Author/game.rb
@@ -1,11 +1,11 @@
 require 'date'
-require_relative './item'
+require './item'
 
 class Game < Item
   attr_accessor :multiplayer, :last_played_at, :publish_date
 
-  def initialize(last_played_at, publish_date, id, archived: false, multiplayer: true)
-    super(publish_date, id, archived: archived)
+  def initialize(multiplayer, last_played_at, publish_date, id = nil, archived: false)
+    super(publish_date, archived: archived)
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
   end
