5cdfe8c6e235f490dcde5525dd6d63f5e0ab64c7
devMunyi <samunyi90@gmail.com>
2023-01-11 09:09:52 +0300

    Fix rubocop flagged errors

--- Book_Label/label.rb
+++ Book_Label/label.rb
@@ -1,8 +1,9 @@
 require_relative './item'
 
 class Label < Item
-  attr_accessor :title, color
-  attr_reader :id, items
+  attr_accessor :title, :color
+  attr_reader :id, :items
+
   def initialize(title, color)
     @id = Random.rand(1..1000)
     @title = title
